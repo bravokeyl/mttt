d77c5f995ffc036be2c23c279289f640d3f1a988
doublepithre <bravokeyl@gmail.com>
2019-10-26 16:55:09 +0100

    feat: init just bottom tabs :boom:

---
+++ src/screens/GenresScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+class GenresScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>GenresScreen!</Text>
+      </View>
+    );
+  }
+}
+
+export default GenresScreen;
---
+++ src/screens/index.js
@@ -0,0 +1,6 @@
+import DiscoverScreen from './DiscoverScreen';
+import GenresScreen from './GenresScreen';
+import MineScreen from './MineScreen';
+import ProfileScreen from './ProfileScreen';
+
+export {DiscoverScreen, GenresScreen, MineScreen, ProfileScreen};
